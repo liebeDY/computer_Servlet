02077778ef11ee5aadf22a2aaed2f50079919977
liebeDY <hdydeveloper0927@gmail.com>
2020-12-22 12:15:16 +0900

    2020.12.22.화요일 #29 수업#3 [Servlet_annotation]

---
+++ 29Day_WebTest3/WebContent/form.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+
+<form action="/29Day_WebTest3/MyServlet" method="post">
+
+name : 
+<input type="text" name="name"> <br>
+<input type="submit" value="전송">
+
+</form>
+</body>
+</html>
---
+++ 29Day_WebTest3/src/step6/MyServlet.java
@@ -0,0 +1,62 @@
+package step6;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet(name ="/MyServlet", urlPatterns = { "/MyServlet" }, 
+initParams = { @WebInitParam(name = "param1", value = "value1"), @WebInitParam(name = "param2", value = "value2") })
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        
+    }
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+		System.out.println(config.getInitParameter("parma1"));
+		System.out.println(config.getInitParameter("parma2"));
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.print("<html><body>name=" + name + "</body></html>");
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
