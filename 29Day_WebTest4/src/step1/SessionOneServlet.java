c21ed25bd05fe13fd47b4a827cb845a41930e0a2
liebeDY <hdydeveloper0927@gmail.com>
2020-12-22 15:26:23 +0900

    2020.12.22.화요일 #29 수업#5 [Servlet_session]

---
+++ 29Day_WebTest4/src/step1/SessionOneServlet.java
@@ -0,0 +1,51 @@
+package step1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionOneServlet
+ */
+@WebServlet("/SessionOneServlet")
+public class SessionOneServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SessionOneServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=uft-8");
+		PrintWriter out = response.getWriter();
+		
+		HttpSession session = request.getSession();
+		// 세션이 새로 생성되면 true, 아니면 false
+		out.print("세션 새로 생성 : " + session.isNew());
+		session.setAttribute("user", "손연재");
+		out.print("<hr><a href=SessionTwoServlet>SessionTwoServlet</a>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
